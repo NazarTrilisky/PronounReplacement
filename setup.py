44b307dd3cb4eefe749f40f230215349c9528fd5
Naz <noreply@github.com>
2022-01-23 16:32:49 -0500

    Setup updates

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 from setuptools import setup
 
 
-setup(name="pronoun_replacement",
+setup(
+      name="pronoun_replacement",
       version='0.0.1',
       description="Pronoun replacement module",
-      url="git@github.com/NazarTrilisky/PronounReplacement.git",
+      url="https://github.com/NazarTrilisky/PronounReplacement",
       install_requires=["spacy==2.3.2"],
       include_package_data=True,
       zip_safe=False,
